cbeca66309e2d516023d681b4822581999cea222
Anton Danilov <adanilovx@gmail.com>
2023-01-26 22:44:58 +0400

    Decision commit

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -4,6 +4,7 @@ public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
         int counter=1;
         int fact=1;
+        System.out.println(1);
         while (counter<=printToInclusive){
             fact=fact*counter;
             System.out.println(fact);
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,17 +4,18 @@ public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive){
 
         if(multiplyByAndToInclusive>0) {
-            int counter=1;
+            int counter=0;
             while (counter <= multiplyByAndToInclusive) {
                 System.out.println(counter * multiplyByAndToInclusive);
                 counter++;
             }
         }
         if(multiplyByAndToInclusive<0){
-            int counter=multiplyByAndToInclusive*(-1);
-            while(counter>multiplyByAndToInclusive&&counter>0){
-                System.out.println(counter*multiplyByAndToInclusive);
-                counter--;
+            int counterStart=0;
+            int counterEnd=multiplyByAndToInclusive*(-1);
+            while(counterStart <=counterEnd){
+                System.out.println(counterStart*multiplyByAndToInclusive);
+                counterStart++;
             }
         }
     }
