566e32e97f7fcfe62bffbb3892afcacf2584093d
Anton Danilov <adanilovx@gmail.com>
2023-01-26 22:54:12 +0400

    Decision commit

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -3,11 +3,9 @@ package school.mjc.stage0.loops.task2;
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int counter=0;
-        String word="";
         while(counter<=chars.length-1){
-            word+=chars[counter];
+            System.out.print(chars[counter]);
             counter++;
         }
-        System.out.println(word);
     }
 }
