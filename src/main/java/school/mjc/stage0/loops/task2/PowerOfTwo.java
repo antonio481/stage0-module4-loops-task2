207df69e66d4d2ac0df96c043d7fb3f15297ba21
Anton Danilov <adanilovx@gmail.com>
2023-01-26 21:44:30 +0400

    Decision commit

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,10 +1,12 @@
 package school.mjc.stage0.loops.task2;
 
+import static java.lang.Math.*;
+
 public class PowerOfTwo {
     public void printPower(int power) {
         int counter=0;
         while(counter<=power){
-            System.out.println(Math.pow(2,power));
+            System.out.println(pow(2,power));
             counter++;
         }
         if(power<0){
