09fe1ae3c430b47a55415b16a2b4b5d15de768b1
Anton Danilov <adanilovx@gmail.com>
2023-01-26 22:36:04 +0400

    Decision commit

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,11 +1,21 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
-    public void printMultiplied(int multiplyByAndToInclusive) {
-        int counter=1;
-        while(counter<=multiplyByAndToInclusive){
-            System.out.println(counter*multiplyByAndToInclusive);
-            counter++;
+    public void printMultiplied(int multiplyByAndToInclusive){
+
+        if(multiplyByAndToInclusive>0) {
+            int counter=1;
+            while (counter <= multiplyByAndToInclusive) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
+        }
+        if(multiplyByAndToInclusive<0){
+            int counter=multiplyByAndToInclusive*(-1);
+            while(counter>multiplyByAndToInclusive&&counter>0){
+                System.out.println(counter*multiplyByAndToInclusive);
+                counter--;
+            }
         }
     }
 }
